916631ae297fcdb3266fa82cda9949fa6eea7e68
Arpithatp <arpithatp03@gmail.com>
2025-08-16 23:05:20 +0530

    Memory Concept completed

---
+++ Day-3/_string.js
@@ -0,0 +1,19 @@
+const name = "Arpitha"
+const repoCount = 3
+console.log(`Hello my name is ${name} and my 
+    github repository count is ${repoCount}`);
+    // Above code is called string interpolation from using this it increses readability and it is modular way
+
+const gameName = new String("Freefire") // another way to define string it has features than normal defination it we can check this in console
+
+console.log(gameName);
+
+console.log(gameName.__proto__);// using this we can use different method like below
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+
+
+
+
+
+
